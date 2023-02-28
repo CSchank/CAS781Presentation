852f72e582bd35213e6deb9e340f39d83e7c4e41
Christopher William Schankula <chris@schankula.ca>
2023-02-28 23:38:06 +0000

    fix type of sqrt in blank

--- app/A1TaglessBlank.hs
+++ app/A1TaglessBlank.hs
@@ -10,7 +10,7 @@ import qualified Prelude (sqrt)
 class Exprs repr where
     const  :: Double -> repr Double
     var    :: String -> repr String
-    sqrt   :: repr a -> repr b
+    sqrt   :: repr Double -> repr Double
     -- uncomment and fill in these type signatures
     -- intPow :: _
     -- exp    :: _
